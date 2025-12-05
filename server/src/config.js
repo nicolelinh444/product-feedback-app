82e05343fce1e2ab55bbd4334dc384363f91f0ec
nicolelinh444 <nicolelinh444@gmail.com>
2025-12-05 09:20:11 -0800

    deleted config.js file

--- server/src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  databaseUrl:
-    "postgresql://neondb_owner:npg_aVBHujkzT3t8@ep-wispy-wildflower-afgf8qf4-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-};
-
-export default config;
